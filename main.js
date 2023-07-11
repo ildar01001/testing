5f1b92b2242d7c5e2148bd091efca864dd83c4ab
ildar01001 <114819499+ildar01001@users.noreply.github.com>
2023-07-12 00:54:12 +0300

    something new

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
+
 const port = 3000;
-app.use('/', function (req, res) {
-    res.send("Hello, Petux!")
-})
+
+app.use('/', express.static('views'));
 
 app.use('/contacts', function(req, res) {
     res.json({
