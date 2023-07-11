4e52245a3ab7a6baf0f9e3b4b917fc677fa26959
Ильдар <ildar123.ru@gmail.com>
2023-07-11 11:14:30 +0300

    added contacts

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ app.use('/', function (req, res) {
     res.send("Hello, Petux!")
 })
 
+app.use('/contacts', function(req, res) {
+    res.json({
+        "Jhon Wick" : "+8756342321",
+        "Mike Wasovsiy" : "+79783"
+    })
+})
 app.listen(port, () => console.log(`Server runs: http://localhost:${port}`));
